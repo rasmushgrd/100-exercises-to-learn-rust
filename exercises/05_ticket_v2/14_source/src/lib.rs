682f580fd9077072cd84ccd551c70f5c21570464
Rasmus Hyldgaard <rasmush2021@protonmail.com>
2026-01-12 12:47:50 +0100

    Completed 05_ticket_v2/14_source

--- exercises/05_ticket_v2/14_source/src/lib.rs
+++ exercises/05_ticket_v2/14_source/src/lib.rs
@@ -1,4 +1,4 @@
-use crate::status::Status;
+use crate::status::{ParseStatusError, Status};
 
 // We've seen how to declare modules in one of the earliest exercises, but
 // we haven't seen how to extract them into separate files.
@@ -23,6 +23,8 @@ pub enum TicketNewError {
     DescriptionCannotBeEmpty,
     #[error("Description cannot be longer than 500 bytes")]
     DescriptionTooLong,
+    #[error("{0}")]
+    StatusInvalid(#[from] ParseStatusError),
 }
 
 #[derive(Debug, PartialEq, Clone)]
@@ -50,9 +52,9 @@ impl Ticket {
         // TODO: Parse the status string into a `Status` enum.
 
         Ok(Ticket {
-            title,
-            description,
-            status,
+            title: title,
+            description: description,
+            status: Status::try_from(status)?,
         })
     }
 }
