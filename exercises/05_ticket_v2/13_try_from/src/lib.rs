f332c0a6cd8490473760e2a8df0886ffbc579134
Rasmus Hyldgaard <rasmush2021@protonmail.com>
2026-01-12 12:47:15 +0100

    Completed 05_ticket_v2/13_try_from

--- exercises/05_ticket_v2/13_try_from/src/lib.rs
+++ exercises/05_ticket_v2/13_try_from/src/lib.rs
@@ -8,6 +8,27 @@ enum Status {
     Done,
 }
 
+impl TryFrom<String> for Status {
+    type Error = String;
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        match value.trim().to_lowercase().as_str() {
+            "todo" | "to-do" | "to do" => Ok(Self::ToDo),
+            "inprogress" | "in-progress" | "in progress" => Ok(Self::InProgress),
+            "done" => Ok(Self::Done),
+            _ => Err("Invalid status".to_string()),
+        }
+    }
+}
+
+impl TryFrom<&str> for Status {
+    type Error = String;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        value.to_string().try_into()
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
